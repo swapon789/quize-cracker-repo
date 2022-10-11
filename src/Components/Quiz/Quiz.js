d2c8ff5b6338159e7ed45b27ca693e636049532e
swapon789 <kumarshopon850@gmail.com>
2022-10-12 01:27:37 +0600

    quiz answer correction

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -3,13 +3,21 @@ import { EyeIcon } from '@heroicons/react/24/solid'
 import { toast } from 'react-toastify';
 
 const Quiz = ({ quize }) => {
-    const { question, correctAnswer } = quize;
+    const { question, correctAnswer, options } = quize;
 
     
     const clickIcon = (correctAnswer) =>{
         toast.success  (correctAnswer, {autoClose: 2000, theme:'dark'})
     }
 
+    const correctAns = (options) => {
+        if (correctAnswer === options){
+            toast.success("Correct Answer", {autoClose: 1000, theme:'dark'});
+        }
+        else{
+            toast.warning('Wrong Answer', {autoClose: 1000, theme:'dark'})
+        }
+    }
 
     return (
         <div>
@@ -24,22 +32,22 @@ const Quiz = ({ quize }) => {
                     </div>
                 </div>
                 <div className="space-y-4  grid items-baseline grid-cols-2">
-                    <div className='border border-blue-700 p-2 m-3 rounded'>
+                    <div onChange={() => correctAns(options[0])} className='border border-blue-700 p-2 m-3 rounded'>
                         <input type="radio" name="java" id="q1" />
                         <label className='ml-3' for="q1">{quize.options[0] ? quize.options[0] : 'Undifined'}</label><br></br>
 
                     </div>
-                    <div className='border border-blue-700 p-2 m-3 rounded'>
+                    <div onChange={() => correctAns(options[1])} className='border border-blue-700 p-2 m-3 rounded'>
                         <input type="radio" name="java" id="q2" />
                         <label className='ml-3' for="q2">{quize.options[1] ? quize.options[1] : 'Undifined'}</label><br></br>
 
                     </div>
-                    <div className='border border-blue-700 p-2 m-3 rounded'>
+                    <div onChange={() => correctAns(options[2])} className='border border-blue-700 p-2 m-3 rounded'>
                         <input type="radio" name="java" id="q3" />
                         <label className='ml-3' for="q3">{quize.options[2] ? quize.options[2] : 'Undifined'}</label><br></br>
 
                     </div>
-                    <div className='border border-blue-700 p-2 m-3 rounded'>
+                    <div onChange={() => correctAns(options[3])} className='border border-blue-700 p-2 m-3 rounded'>
                         <input type="radio" name="java" id="q4" />
                         <label className='ml-3' for="q4">{quize.options[3] ? quize.options[3] : 'Undifined'}</label><br></br>
 
