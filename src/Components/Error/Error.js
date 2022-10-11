8c91d12504ceef16058d6e4a2c818c305d1fcd08
swapon789 <kumarshopon850@gmail.com>
2022-10-12 01:40:56 +0600

    erro button clickback home page

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -12,7 +12,7 @@ const Error = () => {
                         </h2>
                         <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
                         <p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
-                        <Link rel="noopener noreferrer" href="#" className="px-8 py-3 bg-emerald-400 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</Link>
+                        <Link to='/topic' rel="noopener noreferrer" href="#" className="px-8 py-3 bg-emerald-400 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</Link>
                     </div>
                 </div>
             </section>
