0190c0c1d19298ac33de8ccc274162c8d2cbdc91
swapon789 <kumarshopon850@gmail.com>
2022-10-12 00:31:14 +0600

    blog question  add

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -15,6 +15,14 @@ const Blog = () => {
                             <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">How does contex API ?</summary>
                             <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.. </p>
                         </details>
+                        <details className="w-full border rounded-lg bg-blue-500">
+                            <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">Whose href in React?</summary>
+                            <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">This href attribute contains the URL or path to the destination page. It may be a relative URL or an absolute URL. In React, relative URLs should always be handled by the link tag provided by the React Router, and pure anchor tags should only be used for absolute paths.. </p>
+                        </details>
+                        <details className="w-full border rounded-lg bg-blue-500 ">
+                            <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">What is Outlet in react?</summary>
+                            <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">Outlet - A component that renders the next match in a set of matches. Index Route - A child route with no path that renders in the parent's outlet at the parent's URL. Layout Route - A parent route without a path, used exclusively for grouping child routes inside a specific layout. </p>
+                        </details>
                     </div>
                 </div>
             </section>
